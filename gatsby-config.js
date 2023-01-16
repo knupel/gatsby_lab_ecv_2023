95ba875449807104573449b4a9cbe608ad9f8529
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-01-16 12:17:34 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby_Lab`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
