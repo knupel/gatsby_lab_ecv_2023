015a8cd97bf32afe93eb4e422b5afd04a490e259
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2023-02-21 12:53:44 +0100

    push to main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,14 @@
 import * as React from "react"
 
-import { graphql } from "gatsby";
 import Home  from "./home.js"
 
 // Dans le cadre d'une page on peut utiliser le Query global
 
-// const IndexPage = ({data}) => {
-//   return (
-//     <Home data={data}/>
-//   )
-// }
-
 const IndexPage = () => {
   return (
-    <Home/>
+    <Home />
   )
 }
 
 export default IndexPage
 
-export const myQuery = graphql`query {
-  allMarkdownRemark(filter: { frontmatter: { menu: { eq: "accueil" } } }) {
-    edges {
-      node {
-        frontmatter {
-          categorie
-          menu
-          lang
-          title
-          }
-          html
-        }
-      }
-    }
-  }
-`;
